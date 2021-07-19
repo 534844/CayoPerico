c0b9c566fef840420ef17a8be58236a692293470
Dooooc <87667926+Dooooc@users.noreply.github.com>
2021-07-19 20:05:21 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,12 +3,4 @@ fx_version 'adamant'
 
 game 'gta5'
 
-client_scripts {
-	'utils/ragdoll.lua',
-    'utils/sprint.lua',
-    'utils/recoil.lua',
-    'utils/drift.lua',
-    'utils/island.lua',
-    'utils/password.lua',
-    'utils/switcher.lua'
-}
+client_scripts 'utils/island.lua'
