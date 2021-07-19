a2579e5007aa74cdae7ce3395b2305d46d5e7771
Dooooc <87667926+Dooooc@users.noreply.github.com>
2021-07-19 20:04:43 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+version 'v2.5 - Standalone'
+fx_version 'adamant'
+
+game 'gta5'
+
+client_scripts {
+	'utils/ragdoll.lua',
+    'utils/sprint.lua',
+    'utils/recoil.lua',
+    'utils/drift.lua',
+    'utils/island.lua',
+    'utils/password.lua',
+    'utils/switcher.lua'
+}
